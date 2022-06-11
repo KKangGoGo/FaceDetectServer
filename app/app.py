bfe0706cbd24ba5900d71804728ca246ee3c6a41
acisliver <7273322@naver.com>
2022-06-11 20:45:29 +0900

    mq2 ip 환경변수

--- app/app.py
+++ app/app.py
@@ -86,7 +86,7 @@ def send_face_to_mq(album_id, img_url, file_image):
     print(req)
     print('-----mq2에 전송시작-----')
 
-    url = 'http://43.200.5.182/request/siamese'
+    url = 'http://' + cf.MQ_DETECT_SIAMESE_SERVER + '/request/siamese'
     headers = {"Content-Type": "application/json"}
 
     res = requests.post(url, data=json.dumps(req), headers=headers)
@@ -97,7 +97,7 @@ def send_face_to_mq(album_id, img_url, file_image):
 
 # mq2에 작업 확인
 def check(task_id):
-    url = 'http://43.200.5.182/check/'
+    url = 'http://' + cf.MQ_DETECT_SIAMESE_SERVER + '/check/'
     res = requests.get(url + str(task_id))
     print("check " + task_id, bytes.decode(res.content))
     return res.content
